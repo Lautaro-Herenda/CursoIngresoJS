29dc77c39a72cd3cd22b57f571e80f985c605e35
Lautaro-Herenda <60608521+Lautaro-Herenda@users.noreply.github.com>
2020-02-14 12:04:29 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,18 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch (mes) {
+    case "Enero":
+     case "Febrero":
+     case "Marzo":
+     case "Abril":
+     case "Mayo":
+     case "Junio":
+     alert("Falta para el invierno.");
 
-
+}
 
 
 }//FIN DE LA FUNCIÓN
