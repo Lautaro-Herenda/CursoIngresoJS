ef6375572b9a0f7d83d773e854d958bfbc2b13af
Lautaro-Herenda <60608521+Lautaro-Herenda@users.noreply.github.com>
2020-03-02 12:19:55 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,36 @@
 function mostrar()
 {
+var marca;
+var peso;
+var temperatura;
+var seguir;
+var contadorPar;
+var flag;
+var presio;
+var maximo;
+var pesoMax;
+do{
+    marca = prompt("Ingrese una marca: ");
+    peso = parseInt(prompt("ingrese un peso entre 1 y 100"))
+    while (peso < 1 || peso > 100 || isNaN(peso)) {
+        marca = prompt("Eso no es una marca. Ingrese una marca: ");
+    }
+    temperatura = parseInt(prompt("Ingrese un temperatura entre -30 y 30: "));
+    while (temperatura < -30 || temperatura > 30 || isNaN(temperatura)) {
+        temperatura = parseInt(prompt("temperatura invalida. Ingrese un temperatura entre -30 y 30: "));
+}
+    if (temperatura % 2 == 0) {
+        contadorTempPares++;
+
+    if  ((flag == 0 || peso ))             
+
+
+
+
+
+
+
+    }
+
 
 }
--- 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,27 @@
 function mostrar()
 {
+var nombre;
+var edad;
+var sexo;
+var nota;
+var seguir;
+var contadorVar = 0;
+var acumuladorNotasAdolescentes = 0;
+var contadorVaronesAprob
+var 
+var 
+var 
+    do{
+    
+    nombre=prompt("ingrese un nombre: ");
+    edad=parseInt(prompt("ingrese una edad: "));
+    while(isNaN(edad))
+        edad=parseInt(prompt(" invalido, ingrese una edad: "));
+}
+   sexo=prompt("ingrese sexo: ");
+   while(sexo != 'm' && sexo != 'f'){
+       sexo=prompt("valor")
+   }
+    nota=parseInt(prompt("ingrese una nota"));
 
 }
